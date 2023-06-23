0aec58dfe478497257a8bf9b96d44222a5b25f57
WildBarreto <wildsbsrreto@gmail.com>
2023-06-23 14:11:29 -0300

    Padrão module pattern

--- client/app/util/ConnectionFactory.js
+++ client/app/util/ConnectionFactory.js
@@ -1,42 +1,71 @@
 const stores = ['negociacoes'];
-//começa sem conecão
-let connection = null
+let connection = null;
+let close = null;
 
-class ConnectionFactory {
-  constructor() {
-    throw new Error('Não	é	possível	criar	instâncias	dessa	classe');
-  }
-  static getConnection() {
-    return new Promise((resolve, reject) => {
-      if (connection) return resolve(connection)
-      const openRequest = indexedDB.open('jscangaceiro', 2);
-      openRequest.onupgradeneeded = e => {
-        //	PASSA	A	CONEXÃO	PARA	O	MÉTODO
-        ConnectionFactory._createStores(e.target.result);
-      };
-      openRequest.onsuccess = e => {
-        //	passa	o	resultado	(conexão)	para	a	promise!
-        connection = e.target.result
-        resolve(e.target.result);
+const ConnectionFactory = (() => {
 
-      };
-      openRequest.onerror = e => {
-        console.log(e.target.error)
-        //	passa	o	erro	para	reject	da	promise!
-        reject(e.target.error.name)
+  return class ConnectionFactory {
 
-      };
-    });
-  }
-  static _createStores(connection) {
-    stores.forEach(store => {
-      //	if	sem	bloco,	mais	sucinto!
-      if (connection.objectStoreNames.contains(store))
-        connection.deleteObjectStore(store);
-      connection.createObjectStore(store, {
-        autoIncrement:
-          true
+    constructor() {
+
+      throw new Error('Não é possível criar instâncias dessa classe');
+    }
+
+    static getConnection() {
+
+      return new Promise((resolve, reject) => {
+
+        if (connection) return resolve(connection);
+
+        const openRequest = indexedDB.open('jscangaceiro', 2);
+
+        openRequest.onupgradeneeded = e => {
+
+          ConnectionFactory._createStores(e.target.result);
+
+        };
+
+        openRequest.onsuccess = e => {
+
+          connection = e.target.result;
+
+          close = connection.close.bind(connection);
+
+          connection.close = () => {
+            throw new Error('Você não pode fechar diretamente a conexão');
+          };
+
+          resolve(e.target.result);
+
+        };
+
+        openRequest.onerror = e => {
+
+          console.log(e.target.error)
+          reject(e.target.error.name)
+
+        };
+
       });
-    });
+    }
+
+    static _createStores(connection) {
+
+      stores.forEach(store => {
+
+        if (connection.objectStoreNames.contains(store))
+          connection.deleteObjectStore(store);
+
+        connection.createObjectStore(store, { autoIncrement: true });
+      });
+    }
+
+    static closeConnection() {
+
+      if (connection) {
+        close();
+      }
+    }
   }
-}
+
+})();
